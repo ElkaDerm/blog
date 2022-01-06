19b1b5dccc23f08430824313c7f8aa9fcd557bd9
Elena Dermendzhieva <elka.derm@abv.bg>
2022-01-06 16:05:19 +0200

    add default layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,23 +8,25 @@ import { RegisterPage } from "./components/authentication/RegisterPage.js";
 import { AllPosts } from './components/blog-post/AllPosts.js';
 import { DetailsPage } from "./components/blog-post/DetailsPage.js";
 import { HomePage } from "./components/home/HomePage.js";
+import { DefaultLayout } from './components/shared/DefaultLayout.js';
+import { NotFoundPage } from './components/shared/NotFoundPage.js';
 
 
 function App() {
   return (
-    
+
     <Routes>
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path='blog' element={<AllPosts />} />
+        <Route path='blog/:postId' element={<DetailsPage />} />
+        <Route path='register' element={<RegisterPage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route index element={<HomePage />} /> 
+      </Route>
 
-      <Route path='/' element={<HomePage/>} />
-      <Route path='blog' element={<AllPosts/>}/>
-      <Route path='blog/:postId' element={<DetailsPage/>}/>
-      
-      <Route path='register' element={<RegisterPage/>}/>
-      <Route path='login' element={<LoginPage/>}/>
-
-
+      <Route path='*' element={<NotFoundPage/>}/>
     </Routes>
-      
+
 
 
 
---
+++ frontend/src/components/shared/DefaultLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Outlet } from "react-router-dom";
+
+
+export function DefaultLayout () {
+    return (
+        <div>
+            <nav>Navigation</nav>
+            <Outlet/>
+        </div>
+    )
+}
