468fda990916332d7f0bc0626f4561d6e75faa65
Elena Dermendzhieva <elka.derm@abv.bg>
2022-01-11 13:55:36 +0200

    user log in tested

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,7 @@
 const router= require('express').Router();
 
+const User= require('../models/User.js')
+
 
 router.get('/', (req , res) =>{
 
@@ -7,9 +9,17 @@ router.get('/', (req , res) =>{
 
 })
 
-router.post('/sing_in', (req, res) => {
+router.post('/sing_in', async (req, res, next) => {
 console.log ('from backend /users/sing_in');
 console.log (req.body)
+ const username= req.body.username;
+ const password= req.body.password;
+
+const user= await User.findOne({username,password})
+
+    if (!user) {
+        return res.status(404).send()
+    }
     res.status(204).send()
 })
 
